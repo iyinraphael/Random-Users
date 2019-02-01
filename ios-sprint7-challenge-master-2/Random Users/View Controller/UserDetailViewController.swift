6d18d96a56bc1279d4e661cf0c59679d98f420d9
iyinraphael <iyinraphael@gmail.com>
2019-02-01 09:58:35 -0800

    storyboard set up and outlets connected to subviews

---
+++ ios-sprint7-challenge-master-2/Random Users/View Controller/UserDetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UserDetailViewController.swift
+//  Random Users
+//
+//  Created by Iyin Raphael on 2/1/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+   
+    @IBOutlet weak var phoneLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
+    
+}
---
+++ ios-sprint7-challenge-master-2/Random Users/View Controller/UserTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  UserTableViewCell.swift
+//  Random Users
+//
+//  Created by Iyin Raphael on 2/1/19.
+//  Copyright © 2019 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class UserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+}
